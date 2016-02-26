79ea221159caa58d1f65397c9150493d3dec1145
Ng Guoyou <ng.guoyou@gmail.com>
2016-02-26 17:47:07 +0800

    Add contact page

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,19 +3,6 @@ import styles from './index.scss';
 import { config } from 'config';
 import DocumentTitle from 'react-document-title';
 import TechSection from 'components/TechSection';
-import GoogleMap from 'google-map-react';
-
-
-const createMapOptions =
-{
-  scrollwheel: false,
-  styles: [{
-    "featureType": "all",
-    "elementType": "all",
-    "stylers": [{"invert_lightness": true}, {"saturation": 100}, {"lightness": 50}, {"gamma": 0.5}, {"hue": "#EA7A16"}]
-  }]
-};
-
 
 export default () =>
   (
@@ -32,33 +19,25 @@ export default () =>
           </p>
 
           <h1 className={styles.header}> Technology </h1>
-          <TechSection icons={['devicon-javascript-plain',
-          'devicon-react-original',
-          'devicon-d3js-plain',
-          'devicon-ruby-plain',
-          'devicon-rails-plain',
-          'devicon-python-plain',
-          'devicon-linux-plain',
-          'devicon-dot-net',
-          'devicon-android-plain',
-          'devicon-java-plain',
-          'devicon-apple-original',
-          'devicon-redhat-plain',
-          'devicon-apache-plain',
-          'devicon-docker-plain',
-          'devicon-postgresql-plain']}/>
-
-          <div> before</div>
-          <div className={styles.map}>
-            <GoogleMap
-              options={createMapOptions}
-              defaultCenter={{lat: 1.3, lng: 103.789228}}
-              defaultZoom={18}/>
-          </div>
-
-
-          <div> after</div>
-
+          <TechSection
+            icons={[
+              'devicon-javascript-plain',
+              'devicon-react-original',
+              'devicon-d3js-plain',
+              'devicon-ruby-plain',
+              'devicon-rails-plain',
+              'devicon-python-plain',
+              'devicon-linux-plain',
+              'devicon-dot-net',
+              'devicon-android-plain',
+              'devicon-java-plain',
+              'devicon-apple-original',
+              'devicon-redhat-plain',
+              'devicon-apache-plain',
+              'devicon-docker-plain',
+              'devicon-postgresql-plain'
+            ]}
+          />
         </div>
       </div>
     </DocumentTitle>
