f9a4727acfa4b339d92ecdf6096c934e7a17dc51
Ng Guoyou <ng.guoyou@gmail.com>
2016-02-26 17:29:31 +0800

    Consolidate navbar into one component

--- components/TopNav/index.js
+++ components/TopNav/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import NavBar from './NavBar';
+// import NavBar from './NavBar';
 import HeroImage from './HeroImage';
 import { link } from 'gatsby-helpers';
 import { config } from 'config';
 
-const TopNav = ({pathname}) =>
-  (pathname === link('/') ?
-    <HeroImage links={config.navLinks}/> :
-    <NavBar links={config.navLinks}/>);
+const TopNav = ({ pathname }) => (
+  <HeroImage
+    pathname={pathname}
+    links={config.navLinks}
+    fullsize={pathname === link('/')}
+  />
+);
 
 TopNav.propTypes = {
   pathname: React.PropTypes.string
