087be36c0e4df28358ec68be04985512cc8b1ff8
Ng Guoyou <ng.guoyou@gmail.com>
2016-04-14 13:45:38 +0800

    Remove typography.js

--- html.jsx
+++ html.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Typography from 'typography';
 import DocumentTitle from 'react-document-title';
-import { link } from 'gatsby-helpers'
-import { TypographyStyle } from 'utils/typography'
+import { link } from 'gatsby-helpers';
 
 export default class Html extends React.Component {
   render() {
@@ -15,38 +13,29 @@ export default class Html extends React.Component {
     return (
       <html lang="en">
         <head>
-          <meta charSet="utf-8"/>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-          <meta name='viewport' content='user-scalable=no width=device-width, initial-scale=1.0 maximum-scale=1.0'/>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="user-scalable=no width=device-width, initial-scale=1.0 maximum-scale=1.0" />
           <title>{this.props.title}</title>
-          <link rel="shortcut icon" href={this.props.favicon}/>
+          <link rel="shortcut icon" href={this.props.favicon} />
           <link rel="stylesheet" href="//cdn.rawgit.com/konpa/devicon/master/devicon.min.css" />
           <link rel="stylesheet" type="text/css" href={link('/yellow.css')} />
-          <TypographyStyle/>
-          <style dangerouslySetInnerHTML={{__html:
-            `
-              body {
-                color: rgb(66,66,66);
-              }
-              h1,h2,h3,h4,h5,h6 {
-                color: rgb(44,44,44);
-              }
-              a {
-                color: rgb(42,93,173);
-                text-decoration: none;
-              }
-              a:hover {
-                text-decoration: underline;
-              }
-            `
-          }}/>
         </head>
         <body className="landing-page">
-          <div id="react-mount" dangerouslySetInnerHTML={{__html: this.props.body}} />
-          <script src={link("/bundle.js")}/>
+          <div id="react-mount" dangerouslySetInnerHTML={ { __html: this.props.body } } />
+          <script src={link('/bundle.js')} />
         </body>
       </html>
     );
   }
 }
-Html.defaultProps = { body: "" };
+
+Html.defaultProps = {
+  body: ''
+};
+
+Html.propTypes = {
+  body: React.PropTypes.object,
+  favicon: React.PropTypes.string,
+  title: React.PropTypes.string
+};
