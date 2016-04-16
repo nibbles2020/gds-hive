792def959225ad5f05a8bc758fbf3734c3ea0dab
Ng Guoyou <fauxface+github@gmail.com>
2016-04-16 20:06:13 +0800

    Clean up devtool, scss loader configurations

--- gatsby.config.js
+++ gatsby.config.js
@@ -10,55 +10,46 @@ module.exports = function (config, env) {
         browsers: 'last 2 versions',
       })
     ],
-    devtool: env === 'prod' ? null : 'cheap-module-inline-source-map'
+    devtool: (env === 'prod' || env === 'static') ? null : 'cheap-module-inline-source-map'
   });
 
   config.loader('png', function (cfg) {
-    cfg.test = /\.png$/;
+    cfg.test    = /\.png$/;
     cfg.exclude = /node_modules/;
-    cfg.loader = 'url?limit=10000';
+    cfg.loader  = 'url?limit=10000';
     return cfg;
   });
 
   config.loader('jpg', function (cfg) {
-    cfg.test = /\.jpg$/;
+    cfg.test    = /\.jpg$/;
     cfg.exclude = /node_modules/;
-    cfg.loader = 'url?limit=10000';
+    cfg.loader  = 'url?limit=10000';
     return cfg;
   });
 
   config.loader('svg', function (cfg) {
-    cfg.test = /\.svg$/;
+    cfg.test    = /\.svg$/;
     cfg.exclude = /node_modules/;
-    cfg.loader = 'url?limit=10000';
+    cfg.loader  = 'url?limit=10000';
     return cfg;
   });
 
-  if (env === 'static') {
-    config.loader('sass', function (cfg) {
-      cfg.test = /\.scss$/;
-      cfg.loader = ExtractTextPlugin.extract('css?modules&localIdentName=[hash:base64:12]&minimize&importLoaders=1!postcss');
-      return cfg;
-    });
-  } else if (env === 'production') {
-    config.loader('sass', function (cfg) {
-      cfg.test = /\.scss$/;
-      cfg.loader = ExtractTextPlugin.extract('css?modules&localIdentName=[hash:base64:12]&minimize&importLoaders=1!postcss');
-      return cfg;
-    });
-  } else {
-    config.loader('sass', function (cfg) {
-      cfg.test = /\.scss$/;
-      cfg.loader = 'style!css?modules&localIdentName=[path][name]-[local]-[hash:base64:5]&importLoaders=1!postcss';
-      return cfg;
-    });
-    config.devtool = 'cheap-module-inline-source-map';
-  }
+  const sassLoaders = {
+    static:     'css?modules&localIdentName=[hash:base64:12]&minimize&importLoaders=1!postcss',
+    production: 'css?modules&localIdentName=[hash:base64:12]&minimize&importLoaders=1!postcss',
+    otherwise:  'style!css?modules&localIdentName=[path][name]-[local]-[hash:base64:5]&importLoaders=1!postcss'
+  };
 
+  config.loader('sass', function (cfg) {
+    cfg.test    = /\.scss$/;
+    cfg.exclude = /node_modules/;
+    cfg.loader  = ExtractTextPlugin.extract(sassLoaders[env] || sassLoaders.otherwise);
+    return cfg;
+  });
 
-  config.plugin('extract-css',
-    ExtractTextPlugin,
-    ['yellow.css', { allChunks: true }]);
+  config.plugin('extract-css', ExtractTextPlugin,
+    ['yellow.css', { allChunks: true }]
+  );
 
   return config;
 };
