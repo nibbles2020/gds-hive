f14bf1985c981e034acd57bf09f42509e563bf93
Ng Guoyou <fauxface+github@gmail.com>
2016-04-16 19:18:59 +0800

    Slightly clean up Gallery HTML structure

--- components/Gallery/index.js
+++ components/Gallery/index.js
@@ -3,22 +3,21 @@ import container from './GalleryContainer.scss';
 import styles from './GalleryItem.scss';
 import { Link } from 'react-router';
 
-export const GalleryContainer = (props) => {
-  return <div className={container.gallery}>{props.children}</div>;
+export const GalleryContainer = (props) =>
+  <div className={container.gallery}>{props.children}</div>;
+
+GalleryContainer.propTypes = {
+  children: React.PropTypes.element
 };
 
-
-export const GalleryItem = (props) => {
-  return (
-    <div className={styles.wrapper}>
-      <Link to={props.path}>
-        <img className={styles.image} src={props.imgPath} />
-        <p className={styles.title}>{props.caption}</p>
-        <p>{props.synopsis}</p>
-      </Link>
-    </div>
+export const GalleryItem = (props) =>
+  (
+    <Link to={props.path} className={styles.wrapper}>
+      <img className={styles.image} src={props.imgPath} />
+      <p className={styles.title}>{props.caption}</p>
+      <div>{props.synopsis}</div>
+    </Link>
   );
-};
 
 GalleryItem.propTypes = {
   caption: React.PropTypes.string,
