333440086a0144068f4cb54ceed1e0af06996485
Ng Guoyou <fauxface+github@gmail.com>
2016-04-17 00:22:52 +0800

    Fix favicon path for production

--- html.jsx
+++ html.jsx
@@ -16,9 +16,9 @@ const Html = (props) => {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="user-scalable=no width=device-width, initial-scale=1.0 maximum-scale=1.0" />
-        <title>Singapore Government Digital Services</title>
-        <title>{props.title}</title>
-        <link href={favicon} rel="icon" type="image/x-icon" />
+        <title>{props.title || 'Singapore Government Digital Services'}</title>
+        {/* favicon will work for production, and not in dev */}
+        <link href={link(`/${favicon}`)} rel="icon" type="image/x-icon" />
         <link rel="stylesheet" href="//cdn.rawgit.com/konpa/devicon/master/devicon.min.css" />
         <link rel="stylesheet" type="text/css" href={link('/gds.css')} />
         <link href="//fonts.googleapis.com/css?family=Catamaran:500" rel="stylesheet" type="text/css" />
