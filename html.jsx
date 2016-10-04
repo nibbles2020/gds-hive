554d8b769dbd604e7de4ed63c3379f2d74fc440a
Yong Wen Chua <ida@yongwen.xyz>
2016-10-04 15:10:53 +0800

    Fix GA

--- html.jsx
+++ html.jsx
@@ -10,6 +10,15 @@ const Html = (props) => {
     title = props.title;
   }
   // const css = process.env.NODE_ENV === 'production' && <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />;
+  const googleAnalaytics = `"<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-85059701-1', 'auto');
+    ga('send', 'pageview');
+  </script>"`
 
   return (
     <html lang="en">
@@ -27,16 +36,7 @@ const Html = (props) => {
         <link href="//fonts.googleapis.com/css?family=Quattrocento+Sans:400,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Oswald:400,300,700" rel="stylesheet" type="text/css" />
-        
-        <script type="text/javascript">
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-85059701-1', 'auto');
-          ga('send', 'pageview');
-        </script>
+        { googleAnalaytics }
       </head>
 
       <body className="landing-page">
